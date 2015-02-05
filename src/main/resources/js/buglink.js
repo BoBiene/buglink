83a0a52695b544b42fb6a1e84696cc9a954eddef
Sergii Vorushylo <Sergii.Vorushylo@episerver.com>
2015-02-05 12:56:39 +0100

    Use REST API to get commit messages from the current pull request

--- src/main/resources/js/buglink.js
+++ src/main/resources/js/buglink.js
@@ -1,19 +1,41 @@
 (function($) {
     window.episerver = window.episerver || {};
-    episerver.buglink = episerver.buglink || {};
-    episerver.buglink.getBugs = function() {
-        var pr = require('model/page-state').getPullRequest(), links = [];
-        if (pr && pr.attributes) {
+    window.episerver.buglink = episerver.buglink || {};
+    window.episerver.buglink.getBugs = function() {
+        var pr = require('model/page-state').getPullRequest(),
+            project = require('model/page-state').getProject(),
+            repository = require('model/page-state').getRepository(),
+            links = [];
+
+        if (pr && pr.attributes
+            && project && project.attributes
+            && repository && repository.attributes) {
             if (pr.attributes.title) {
                 links = links.concat(getLinks(pr.attributes.title));
             }
             if (pr.attributes.description) {
                 links = links.concat(getLinks(pr.attributes.description));
             }
-            if (pr.attributes.fromRef && pr.attributes.fromRef.attributes && pr.attributes.fromRef.attributes.displayId) {
+            if (pr.attributes.fromRef && pr.attributes.fromRef.attributes
+                && pr.attributes.fromRef.attributes.displayId) {
                 links = links.concat(getLinks(pr.attributes.fromRef.attributes.displayId));
             }
+
+            var url = "/stash/rest/api/1.0/projects/"+project.attributes.key
+                        +"/repos/"+repository.attributes.slug
+                        +"/pull-requests/"+pr.attributes.id
+                        +"/commits?withCounts=true";
+            $.ajax({
+                url: url,
+                success: function(data) {
+                    $.each(data.values, function(i, commit) {
+                        links = links.concat(getLinks(commit.message));
+                    })
+                },
+                async: false // TODO: Consider async: true, when find how to render SOY template with async data
+            });
         }
+
         var seen={}, uniqueLinks = $.grep(links, function(l) {
             if (seen[l.title]) {
                 return false;
@@ -25,8 +47,9 @@
     };
 
     function getLinks(text) {
-        var regex = /[#\/](\d+)/g, pattern = "http://tfs01vm:8080/tfs/web/UI/Pages/WorkItems/WorkItemEdit.aspx?id={$id}";
-        var match = [], result = [];
+        var regex = /[#\/](\d+)/g, // TODO: Move these parameters to configuration
+            pattern = "http://tfs01vm:8080/tfs/web/UI/Pages/WorkItems/WorkItemEdit.aspx?id={$id}",
+            match = [], result = [];
         while(match = regex.exec(text)) {
             var href = pattern.replace("{$id}", match[1]);
             var title = "#"+match[1];
